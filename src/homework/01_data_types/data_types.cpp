4b2972e19307b2cf723baa2531224282df338fea
cesarsilveira1 <cesar.silveira@g.austincc.edu>
2024-09-10 00:30:24 +0000

    homework change

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -9,5 +9,13 @@ int add_numbers(int num1, int num2)
 //write function code here
 
 
+//write namespace using statement for cout
+int multiply_numbers(int num1);
 
+{
+	int num2=5;
+	int product = num1*num2;
+
+	return product;
+}
 
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,23 @@
 ﻿//write include statements
-
+#include "data_types.h"// tells c++ where to finda multiply numbers function
+#include<iostream>
 
 //write namespace using statement for cout
-
+using std::cout; using std::cin;
 
 int main()
 {
-	
+	int num;
+	cout<<"Enter a Number";
+	cin>>num;
 
+	int result = multiply_numbers(num);
+	cout<< "results: "<<result<<"\n";
+	
+	int num1=4;
+
+	result = multiply_numbers(num1);
+	cout<< "results: "<<result<<"\n"; 
+	
 	return 0;
 }
