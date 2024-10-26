7e73f5e905c83b5aba0ba79ebf0a2954e3159887
cesarsilveira1 <cesar.silveira@g.austincc.edu>
2024-10-26 15:54:25 -0500

    Update main.cpp

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
+#include "tic_tac_toe.h"
 
-int main() 
-{
+int main() {
+    TicTacToe game;
+    std::string first_player;
+    char choice;
 
-	return 0;
+    do {
+        std::cout << "Enter first player (X or O): ";
+        std::cin >> first_player;
+        game.start_game(first_player);
+
+        while (!game.game_over()) {
+            game.display_board();
+            int position;
+            std::cout << "Enter position (1-9): ";
+            std::cin >> position;
+            game.mark_board(position);
+        }
+
+        game.display_board();
+        std::cout << "Game over! Would you like to play again? (y/n): ";
+        std::cin >> choice;
+
+    } while (choice == 'y');
+
+    return 0;
 }
